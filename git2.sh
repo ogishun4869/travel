15da894eff9a932660ed6d18e5e896fdf510a2b0
mtbys <mtbys@MacBook-Air.local>
2024-09-28 12:40:27 +0900

    first commit

--- git2.sh
+++ git2.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 git init
+git remote add origin git@github.com:ogishun4869/travel.git
 git add .
 git commit -m "first commit"
-git branch -M main  # ブランチ名を okayama に変更
-git remote add origin git@github.com:ogishun4869/travel.git
 git push -u origin main
 
